aa7cf220d5c4dc00b8172963ab654b743060bb82
aidqen <idanmarkin8@gmail.com>
2024-05-05 13:24:44 +0300

    Add routing and modified bookService

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,15 +1,30 @@
+const { useState } = React
+
 import { About } from "./pages/About.jsx";
 import { BookIndex } from "./pages/BookIndex.jsx";
 import { Home } from "./pages/Homepage.jsx";
 
 
 export function RootCmp() {
+
+    const [ route, setRoute ] = useState('bookIndex')
+
   return (
     <main className="content-grid">
-      
-      <Home />
-      <BookIndex />
-      <About /> 
+      <header>
+        <h1>Miss Books</h1>
+        <nav>
+            <ul>
+                <li onClick={() => setRoute('home')}>Home</li>
+                <li onClick={() => setRoute('bookIndex')}>Book Index</li>
+                <li onClick={() => setRoute('about')}>About</li>
+            </ul>
+        </nav>
+      </header>
+
+    {route === 'home' && <Home />}
+    {route === 'bookIndex' && <BookIndex />}
+    {route === 'about' && <About />}
     </main>
   )
 }
--- services/book.service.js
+++ services/book.service.js
@@ -5,131 +5,101 @@ const BOOK_KEY = 'bookDB'
 _createbooks()
 
 export const bookService = {
-    query,
-    get,
-    remove,
-    save,
-    getEmptyBook,
-    getDefaultFilter,
-    getSpeedStats,
-    getVendorStats
+  query,
+  get,
+  remove,
+  save,
+  getEmptyBook,
+  getDefaultFilter,
+
 }
 // For Debug (easy access from console):
 // window.cs = bookService
 
 function query(filterBy = {}) {
-    return storageService.query(BOOK_KEY)
-        .then(books => {
-            if (filterBy.txt) {
-                const regExp = new RegExp(filterBy.txt, 'i')
-                books = books.filter(book => regExp.test(book.vendor))
-            }
+  return storageService.query(BOOK_KEY).then(books => {
+    if (filterBy.txt) {
+      const regExp = new RegExp(filterBy.txt, 'i')
+      books = books.filter(book => regExp.test(book.vendor))
+    }
 
-            if (filterBy.minSpeed) {
-                books = books.filter(book => book.maxSpeed >= filterBy.minSpeed)
-            }
+    if (filterBy.minSpeed) {
+      books = books.filter(book => book.maxSpeed >= filterBy.minSpeed)
+    }
 
-            return books
-        })
+    return books
+  })
 }
 
 function get(bookId) {
-    return storageService.get(BOOK_KEY, bookId)
-        .then(book => {
-            book = _setNextPrevbookId(book)
-            return book
-        })
+  return storageService.get(BOOK_KEY, bookId).then(book => {
+    book = _setNextPrevbookId(book)
+    return book
+  })
 }
 
 function remove(bookId) {
-    return storageService.remove(BOOK_KEY, bookId)
+  return storageService.remove(BOOK_KEY, bookId)
 }
 
 function save(book) {
-    if (book.id) {
-        return storageService.put(BOOK_KEY, book)
-    } else {
-        return storageService.post(BOOK_KEY, book)
-    }
+  if (book.id) {
+    return storageService.put(BOOK_KEY, book)
+  } else {
+    return storageService.post(BOOK_KEY, book)
+  }
 }
 
-function getEmptybook(vendor = '', maxSpeed = '') {
-    return { vendor, maxSpeed }
+function getEmptybook(ctg = '', maxSpeed = '') {
+  return { vendor, maxSpeed }
 }
 
 function getDefaultFilter(filterBy = { txt: '', minSpeed: 0 }) {
-    return { txt: filterBy.txt, minSpeed: filterBy.minSpeed }
+  return { txt: filterBy.txt, minSpeed: filterBy.minSpeed }
 }
 
-function getSpeedStats() {
-    return storageService.query(BOOK_KEY)
-        .then(books => {
-            const bookCountBySpeedMap = _getbookCountBySpeedMap(books)
-            const data = Object.keys(bookCountBySpeedMap).map(speedName => ({ title: speedName, value: bookCountBySpeedMap[speedName] }))
-            return data
-        })
-
-}
-
-function getVendorStats() {
-    return storageService.query(BOOK_KEY)
-        .then(books => {
-            const bookCountByVendorMap = _getbookCountByVendorMap(books)
-            const data = Object.keys(bookCountByVendorMap)
-                .map(vendor =>
-                ({
-                    title: vendor,
-                    value: Math.round((bookCountByVendorMap[vendor] / books.length) * 100)
-                }))
-            return data
-        })
-}
-
-function _createbooks() {
-    let books = utilService.loadFromStorage(BOOK_KEY)
-    if (!books || !books.length) {
-        books = []
-        const vendors = ['audu', 'fiak', 'subali', 'mitsu']
-        for (let i = 0; i < 6; i++) {
-            const vendor = vendors[utilService.getRandomIntInclusive(0, vendors.length - 1)]
-            books.push(_createbook(vendor, utilService.getRandomIntInclusive(80, 300)))
-        }
-        utilService.saveToStorage(BOOK_KEY, books)
+function _createBooks() {
+  const ctgs = ['Love', 'Fiction', 'Poetry', 'Computers', 'Religion']
+  const books = []
+  for (let i = 0; i < 20; i++) {
+    const book = {
+      id: utilService.makeId(),
+      title: utilService.makeLorem(2),
+      subtitle: utilService.makeLorem(4),
+      authors: [utilService.makeLorem(1)],
+      publishedDate: utilService.getRandomIntInclusive(1950, 2024),
+      description: utilService.makeLorem(20),
+      pageCount: utilService.getRandomIntInclusive(20, 600),
+      categories: [ctgs[utilService.getRandomIntInclusive(0, ctgs.length - 1)]],
+      thumbnail: `http://coding-academy.org/books-photos/${i + 1}.jpg`,
+      language: 'en',
+      listPrice: {
+        amount: utilService.getRandomIntInclusive(80, 500),
+        currencyCode: 'EUR',
+        isOnSale: Math.random() > 0.7,
+      },
     }
+    books.push(book)
+  }
+  console.log('books', books)
 }
 
-function _createbook(vendor, maxSpeed = 250) {
-    const book = getEmptybook(vendor, maxSpeed)
-    book.id = utilService.makeId()
-    return book
-}
+// function _createbook(vendor, maxSpeed = 250) {
+//   const book = getEmptybook(vendor, maxSpeed)
+//   book.id = utilService.makeId()
+//   return book
+// }
 
 function _setNextPrevbookId(book) {
-    return storageService.query(BOOK_KEY).then((books) => {
-        const bookIdx = books.findIndex((currbook) => currbook.id === book.id)
-        const nextbook = books[bookIdx + 1] ? books[bookIdx + 1] : books[0]
-        const prevbook = books[bookIdx - 1] ? books[bookIdx - 1] : books[books.length - 1]
-        book.nextbookId = nextbook.id
-        book.prevbookId = prevbook.id
-        return book
-    })
+  return storageService.query(BOOK_KEY).then(books => {
+    const bookIdx = books.findIndex(currbook => currbook.id === book.id)
+    const nextbook = books[bookIdx + 1] ? books[bookIdx + 1] : books[0]
+    const prevbook = books[bookIdx - 1]
+      ? books[bookIdx - 1]
+      : books[books.length - 1]
+    book.nextbookId = nextbook.id
+    book.prevbookId = prevbook.id
+    return book
+  })
 }
 
-function _getbookCountBySpeedMap(books) {
-    const bookCountBySpeedMap = books.reduce((map, book) => {
-        if (book.maxSpeed < 120) map.slow++
-        else if (book.maxSpeed < 200) map.normal++
-        else map.fast++
-        return map
-    }, { slow: 0, normal: 0, fast: 0 })
-    return bookCountBySpeedMap
-}
-
-function _getbookCountByVendorMap(books) {
-    const bookCountByVendorMap = books.reduce((map, book) => {
-        if (!map[book.vendor]) map[book.vendor] = 0
-        map[book.vendor]++
-        return map
-    }, {})
-    return bookCountByVendorMap
-}
