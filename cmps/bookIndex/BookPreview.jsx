e3828f89f61fc66e9dfd3db36c107f725340aa90
aidqen <idanmarkin8@gmail.com>
2024-05-06 11:13:34 +0300

    Finish BookPreview

--- cmps/bookIndex/BookPreview.jsx
+++ cmps/bookIndex/BookPreview.jsx
@@ -1,14 +1,19 @@
-export function BookPreview({book}) {
-    const { title, authors, subtitle, publishedDate, pageCount, categories} = book 
+export function BookPreview({ book }) {
+  const { title, authors, subtitle, publishedDate, pageCount, categories, listPrice } = book
 
-    return (
-        <tr>
-                <td>{title}</td>
-                <td>{authors.join(' ')}</td>
-                <td>{subtitle}</td>
-                <td>{publishedDate}</td>
-                <td>{pageCount}</td>
-                <td>{categories}</td>
-        </tr>
-    )
+  return (
+    <tr>
+      <td>{title}</td>
+      <td>{authors.join(' ')}</td>
+      <td>{subtitle}</td>
+      <td>{publishedDate}</td>
+      <td>{pageCount}</td>
+      <td>{categories}</td>
+      <td className={listPrice.isOnSale ? 'bold' : ''}>
+        {`${listPrice.amount}${listPrice.currencyCode} ${
+          listPrice.isOnSale ? 'On Sale!' : ''
+        }`}
+      </td>
+    </tr>
+  )
 }
