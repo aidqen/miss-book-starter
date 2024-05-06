a68be1fe44cffc4e7745fbb4491f566545c926e3
aidqen <idanmarkin8@gmail.com>
2024-05-06 09:37:07 +0300

    Add cmp pages and finish initial BookIndex

--- pages/BookIndex.jsx
+++ pages/BookIndex.jsx
@@ -1,7 +1,30 @@
+import { BookList } from '../cmps/bookIndex/BookList.jsx';
 import { bookService } from '../services/book.service.js'
+const { useState, useEffect } = React
 
 export function BookIndex() {
-    bookService.query().then(console.log)
-
-    return 
+    const [books, setBooks] = useState([]);
+    
+    useEffect(() => {
+        bookService.query().then(booksPrm => setBooks(booksPrm));
+    }, []);
+    
+    
+  return (
+    <table>
+        <thead>
+            <tr>
+                <td>Title:</td>
+                <td>Authors:</td>
+                <td>Subtitle:</td>
+                <td>Publish Date:</td>
+                <td>Page Count:</td>
+                <td>Categories:</td>
+            </tr>
+        </thead>
+        <tbody>
+            <BookList books={books}/>
+        </tbody>
+    </table>
+  )
 }
